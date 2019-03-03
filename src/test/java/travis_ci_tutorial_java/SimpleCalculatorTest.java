580b6a159c57e415b64332607f67fc75c6d88135
youssoufdembele <youssoufdembele@10.0.0.60>
2019-03-03 18:06:30 -0500

    Pushing updated results with error with tesMinus class in SimpleCalculatorTest without making any change in the SimpleCalculator

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,11 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2);
+	}
+	
+	
 }
